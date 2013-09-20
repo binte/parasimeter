02792e1d08da2aa57bc32d27a2132c1c6d6267fa
nuno <nuno@Nuno-2.local>
2013-09-20 17:16:15 +0100

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parasimeter::Application.config.secret_key_base = '9e592e734e86e88508c8f705b75719554ec3939e10117207f38a6bed9c49dd43cf6d29c34377500404a468d7322a490f96a2cc03f510e03e0c248ae63adb6bde'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parasimeter::Application.config.session_store :cookie_store, key: '_parasimeter_session'
