23689457d71c8d46fbd12bcc471115c09687ccee
luisnunobarbosa <nuno@Nuno-2.local>
2013-09-26 18:12:08 +0100

    messages updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,33 @@
 
 #100% TRABALHO
 Phrase.create(text: "You liar...", min: 0, max: 0)
-Phrase.create(text: "AHAHA Nice try! Go fuck with someone else dude", min: 0, max: 0)
-Phrase.create(text: "Do you get laid?", min: 0, max: 0)
+Phrase.create(text: "AHAHA Nice try! Go fuck someone else dude!", min: 0, max: 0)
+Phrase.create(text: "Do you ever get laid, faggot?", min: 0, max: 0)
 
 #ATÉ 32% PARASITISMO
-Phrase.create(text: "Go away! This site is not for you!", min: 1, max: 32)
-Phrase.create(text: "I have to practice my procrastinating skills", min: 1, max: 32)
-Phrase.create(text: "Get yourself a life!", min: 1, max: 32)
+Phrase.create(text: "Go away! This site is for procrastinators!", min: 1, max: 32)
+Phrase.create(text: "You have to practice your procrastinating skills!", min: 1, max: 32)
+Phrase.create(text: "Get a life, faggot!", min: 1, max: 32)
 
 #ATÉ 65% PARASITISMO
-Phrase.create(text: "I'm a really moderate guy!", min: 33, max: 65)
-Phrase.create(text: "I don't want to play with you anymore", min: 33, max: 65)
+Phrase.create(text: "You're a really balanced guy! But procrastinators are happier than balanced guys!", min: 33, max: 65)
+Phrase.create(text: "I don't wanna play with you anymore! You spend too much time working!", min: 33, max: 65)
 
 #ATÉ 99% PARASITISMO
-Phrase.create(text: "Today I parasited like a mother fuc#er BOSS", min: 66, max: 99)
+Phrase.create(text: "Today you parasited like a motherfucking BOSS", min: 66, max: 99)
 Phrase.create(text: "Fuck you, workers. Long Live Procrastination!", min: 66, max: 99)
-Phrase.create(text: "I'm a PRO crastinator", min: 66, max: 99)
 Phrase.create(text: "PROCRASTINATION is like MASTURBATION! I know I'm screwing myself, but it feels so fucking good", min: 66, max: 99)
-Phrase.create(text: "If my mother was my boss, I had already been fired a million times", min: 66, max: 99)
+Phrase.create(text: "If your mother was your boss, you had already been fired a million times", min: 66, max: 99)
 Phrase.create(text: "Today a procrastinator, tomorrow a world leader", min: 66, max: 99)
-Phrase.create(text: "Procrastination: my school for politics", min: 66, max: 99)
-Phrase.create(text: "I live in Procrasti NATION", min: 66, max: 99)
-Phrase.create(text: "Para... Para... Parasite", min: 66, max: 99)
-Phrase.create(text: "Parasiting Activity", min: 66, max: 99)
+Phrase.create(text: "Procrastination: your school to politics", min: 66, max: 99)
+Phrase.create(text: "You live in Procrasti NATION", min: 66, max: 99)
+Phrase.create(text: "Para... Para... Parasite!", min: 66, max: 99)
+Phrase.create(text: "PARA PA PARA... A PARASITE!", min: 66, max: 99)
+Phrase.create(text: "Today a procrastinator, tomorrow the Prime Minister", min: 66, max: 99)
 
 #100% PARASITA
 Phrase.create(text: "We have a winner!!!", min: 100, max: 100)
-Phrase.create(text: "Houston, we have a parasite", min: 100, max: 100)
-Phrase.create(text: "Today I didn't do a fucking thing", min: 100, max: 100)
-Phrase.create(text: "Training to be a politician. I didn't do a fuck all day", min: 100, max: 100)
+Phrase.create(text: "Houston, we have a parasite!", min: 100, max: 100)
+Phrase.create(text: "You are a full parasite!", min: 100, max: 100)
+Phrase.create(text: "Training to be a politician? You didn't do a fuck all day", min: 100, max: 100)
+Phrase.create(text: "You're a PRO... crastinator!", min: 100, max: 100)
